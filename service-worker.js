008bd0b7c2599c2524533f83f78e402dda8a1717
allbonykm <allbony@olbone.com>
2025-12-11 15:50:31 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'yul-apps-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/gecko.png',
+  '/coin.png',
+  '/bike.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
